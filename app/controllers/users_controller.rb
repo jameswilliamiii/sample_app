69e453ecadc8c9d2f58e9cd35ac2934aaa77be0e
James Stubblefield <jameswilliamiii@gmail.com>
2012-05-31 01:42:13 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
   
   def create
-    @user = User.new params[:user]
+    if signed_in?
+      redirect_to root_path
+    else
+      @user = User.new params[:user]
       if @user.save
         sign_in @user
         flash[:success] = "Welcome to the Sample App!"
@@ -12,6 +28,7 @@ class UsersController < ApplicationController
       else
         render 'new'
       end
+    end
   end
   
   def show
@@ -19,12 +36,46 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find params[:id]
+
   end
   
   def update
-    
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
+  def destroy
+    user = User.find(params[:id])
+    if user == current_user
+        redirect_to users_path, notice: 'Admins cannot delete their own account.'
+    else
+      user.destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_path
+    end
+  end
+  
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+      
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
   
 end
